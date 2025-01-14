d41957290a26d6337ce4477283a9153b5e69a27d
SHervas03 <sergiohervas9rgmail.com>
2025-01-14 20:00:59 +0100

    exercises 4, 5 and 6

--- main.py
+++ main.py
@@ -68,8 +68,29 @@ def renaming_values(df):
 
     return df
 
+def null_treatment(df):
+    df.dropna(axis=1, how="all", inplace=True)
+    df['positiva_droga']=df['positiva_droga'].fillna(0)
+    df['positiva_alcohol']=df['positiva_alcohol'].fillna('N')
+    df['lesividad']=df['lesividad'].fillna('Sin atención sanitaria')
+    df['cod_lesividad']=df['cod_lesividad'].fillna(0)
+    df['estado_meteorológico']=df['estado_meteorológico'].fillna('Se desconoce')
+    df.dropna(inplace=True)
+    return df
 
 
+def restructuringByPercentage(df):
+    frequency = df['tipo_accidente'].value_counts(normalize=True)*100
+    minor_categories = frequency[frequency < 10].index
+    df['tipo_accidente'] = df['tipo_accidente'].replace(minor_categories, "Other categories")
+    return df
+
+def alcohol_and_drug_testing(df):
+    positiveResults = df[(df['positiva_alcohol'] == "S") & (df['positiva_droga'] == 1)]
+    involved = positiveResults.groupby("num_expediente").size().sum()
+    files = positiveResults['num_expediente'].nunique()
+    print("Many involved in alcohol and drugs: ", involved)
+    print("Different files: ", files)
 
 
 if __name__ == "__main__":
@@ -79,16 +100,24 @@ if __name__ == "__main__":
 
     if os.path.exists(origin_df):
         df = pd.read_excel(origin_df)
-        print(1)
     else:
         df = reading_and_unifying_files(years=years)
         df.to_excel(origin_df, index=False)
-        print(2)
 
     df.drop(['coordenada_x_utm', 'coordenada_y_utm'], axis=1, inplace=True)
 
+    # print(df['tipo_vehiculo'].unique)
+
     df_renaming = renaming_values(df=df)
 
-    print(df['tipo_vehiculo'].unique())
+    print("dataframe before cleaning: ",df.shape)
+    df_clean = null_treatment(df_renaming)
+    print("dataframe after cleaning: ",df.shape)
+
+    df_percentage = restructuringByPercentage(df_clean)
+
+    alcohol_and_drug_testing(df_percentage)
+
+
 
 
